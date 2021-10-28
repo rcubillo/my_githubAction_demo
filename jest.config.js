28f6e87562199e7d53d6083538fbd58cd4075db1
parviz05 <51834943+parviz05@users.noreply.github.com>
2021-10-27 22:40:44 -0500

    junit.xml

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,35 @@
-module.exports = {
-    globalSetup: './setup.js',
-    globalTeardown: './teardown.js',
-    testEnvironment: 'jest-environment-node',
-    reporters: [ "default",
-         ["jest-junit", {"classNameTemplate": "e2e","titleTemplate": "{api.test} {title}"}]
-        ]
-  }
+// module.exports = {
+//     globalSetup: './setup.js',
+//     globalTeardown: './teardown.js',
+//     testEnvironment: 'jest-environment-node',
+//     reporters: [
+//         "default",
+//           [ "jest-junit", { suiteName: "api-test" } ]
+//     ]
+//   }
+
+//   module.exports = async () => {
+//     return {
+
+//         globalSetup: './setup.js',
+//         globalTeardown: 'jest.setup.js',
+//         testEnvironment: 'jest-environment-node',
+//         reporters: [
+//             "default",
+//               [ "jest-junit", { suiteName: "api-test" } ]
+//         ]
+//     };
+//   };
+
+const config = {
+    verbose: true,
+    node_modules: './setup.js',
+    globalConfig: 'jest.setup.js',
+            testEnvironment: 'jest-environment-node',
+            reporters: [
+                "default",
+                  [ "jest-junit", { suiteName: "api-test" } ]
+            ] 
+  };
+
+  module.exports = config;
