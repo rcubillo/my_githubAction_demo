1f0205d0eaad54dd0a50d6136d4509e1fdef7e3d
parviz05 <51834943+parviz05@users.noreply.github.com>
2021-11-03 14:35:47 -0500

    Added Sanity Suite

---
+++ api-test/sanitySuite/apiSample.test.js
@@ -0,0 +1,34 @@
+
+const supertest = require('supertest')
+const request = supertest('https://reqres.in/')
+
+
+
+
+const TestData = require('../../test_data/data.json')
+
+
+
+describe('API DEMO Test', () => {
+
+    beforeAll(() => {
+
+        console.log('Run befor Script');
+    });
+    afterAll(() => {
+        console.log('Run after Script ');
+    });
+
+    test.only.each(TestData.user)('Post-Create a new user (%s)', async (user) => {
+
+        const response = await request.post('api/users').send(user)
+        expect(response.status).toBe(201);
+        expect(response.body.name).toBe(user.name)
+        console.log(response.body)
+
+
+    });
+   
+});
+
+
