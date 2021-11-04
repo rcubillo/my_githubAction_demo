840c88b337c55b6a428e0e7d0b3f8b96eeb1cfec
parviz05 <51834943+parviz05@users.noreply.github.com>
2021-11-04 13:13:52 -0500

    Update apiSample.test.js

--- api-test/sanitySuite/apiSample.test.js
+++ api-test/sanitySuite/apiSample.test.js
@@ -22,7 +22,7 @@ describe('API DEMO Test', () => {
     test.each(TestData.user)('Post-Create a new user (%s)', async (user) => {
 
         const response = await request.post('api/users').send(user)
-        expect(response.status).toBe(201);
+        expect(response.status).toBe(501);
         expect(response.body.name).toBe(user.name)
         console.log(response.body)
 
